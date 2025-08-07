54cb201843060f2bbb1c895d9ebb3bef8d37dc35
ToukaSama <blacksummoner6659@gmail.com>
2025-08-07 20:06:28 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -194,6 +194,7 @@ async def not_joined(client: Client, message: Message):
         pass
 
     await message.reply(
+        photo=FORCE_PIC,
         text = FORCE_MSG.format(
                 first = message.from_user.first_name,
                 last = message.from_user.last_name,
